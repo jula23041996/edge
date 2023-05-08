eb7aa43e6ffd5da85b2268ff59058f0569107922
jula23041996 <130248094+jula23041996@users.noreply.github.com>
2023-05-08 16:11:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -281,38 +281,6 @@ if (window.matchMedia('(max-width: 1024px)').matches) {
     part.addEventListener('mouseleave', toggleDisplay);
   });
 }
-  
-//Toggling for parts 
-const toggleParts = document.querySelectorAll('.sustainability__part');
-
-function toggleDisplay() {
-  // Get the next sibling element of the clicked div
-  const targetPart = this.firstElementChild;
-  const targetPart2 = this.lastElementChild;
-  // Toggle the display of the target div
-  if (targetPart.style.display === 'none') {
-    targetPart.style.display = 'block';
-    targetPart2.style.display = 'none';
-  } else {
-    targetPart.style.display = 'none';
-    targetPart2.style.display = 'block';
-  }
-}
-
-if (window.matchMedia('(max-width: 1024px)').matches) {
-  toggleParts.forEach(function(part) {
-    part.addEventListener('click', toggleDisplay);
-    part.removeEventListener('mouseenter', toggleDisplay);
-    part.removeEventListener('mouseleave', toggleDisplay);
-  });
-} else {
-  toggleParts.forEach(function(part) {
-    part.removeEventListener('click', toggleDisplay);
-    part.addEventListener('mouseenter', toggleDisplay);
-    part.addEventListener('mouseleave', toggleDisplay);
-  });
-}
-
 // Get all the dropdown buttons
 const dropdownButtons2 = document.querySelectorAll('.item__subtitle');
 
@@ -494,3 +462,171 @@ let myImageSlider2 = new Swiper('#swiper-container2', {
 	observeSlideChildren: true,
 
 });
+// Инициализируем Swiper
+let myImageSlider3 = new Swiper('#swiper-container3', {
+	// Стрелки
+	navigation: {
+		nextEl: '.next3',
+		prevEl: '.previous3'
+	},
+
+	// Включение/отключение
+	// перетаскивания на ПК
+	simulateTouch: true,
+	// Чувствительность свайпа
+	touchRatio: 1,
+	// Угол срабатывания свайпа/перетаскивания
+	touchAngle: 45,
+	// Курсор перетаскивания
+	grabCursor: true,
+
+	// Переключение при клике на слайд
+	slideToClickedSlide: false,
+
+	// Навигация по хешу
+	hashNavigation: {
+		// Отслеживать состояние
+		watchState: true,
+	},
+
+	// Управление клавиатурой
+	keyboard: {
+		// Включить\выключить
+		enabled: true,
+		// Включить\выключить
+		// только когда слайдер
+		// в пределах вьюпорта
+		onlyInViewport: true,
+		// Включить\выключить
+		// управление клавишами
+		// pageUp, pageDown
+		pageUpDown: true,
+	},
+
+	// Управление колесом мыши
+	mousewheel: {
+		// Чувствительность колеса мыши
+		sensitivity: 1,
+		// Класс объекта на котором
+		// будет срабатывать прокрутка мышью.
+		//eventsTarget: ".image-slider"
+	},
+
+	// Автовысота
+	autoHeight: false,
+
+	// Количество слайдов для показа
+	slidesPerView: 3,
+
+	// Отключение функционала
+	// если слайдов меньше чем нужно
+	watchOverflow: true,
+
+	// Отступ между слайдами
+	spaceBetween: 30,
+
+	// Количество пролистываемых слайдов
+	slidesPerGroup: 1,
+
+	// Активный слайд по центру
+	centeredSlides: true,
+
+	// Стартовый слайд.
+	initialSlide: 1,
+
+	// Мультирядность
+	slidesPerColumn: 1,
+
+	// Бесконечный слайдер
+	loop: false,
+
+	// Кол-во дублирующих слайдов
+	loopedSlides: 0,
+
+	// Свободный режим
+	freeMode: true,
+	
+
+	// Скорость
+	speed: 800,
+
+	// Вертикальный слайдер
+	direction: 'horizontal',
+
+	
+	// Эффекты переключения слайдов.
+	// Листание
+	effect: 'slide',	
+	// Брейк поинты (адаптив)
+	// Ширина экрана
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		480: {
+			slidesPerView: 1.5,
+		},
+		992: {
+			slidesPerView: 2,
+		},
+		1024: {
+			slidesPerView: 3,
+		},
+	},
+
+	/*
+	// Брейк поинты (адаптив)
+	// Соотношение сторон
+	breakpoints: {
+		'@0.75': {
+			slidesPerView: 1,
+		},
+		'@1.00': {
+			slidesPerView: 2,
+		},
+		'@1.50': {
+			slidesPerView: 3,
+		}
+	},
+	*/
+
+	// Отключить предзагрузка картинок
+	preloadImages: false,
+	// Lazy Loading
+	// (подгрузка картинок)
+	lazy: {
+		// Подгружать на старте
+		// переключения слайда
+		loadOnTransitionStart: false,
+		// Подгрузить предыдущую
+		// и следующую картинки
+		loadPrevNext: false,
+	},
+	// Слежка за видимыми слайдами
+	watchSlidesProgress: true,
+	// Добавление класса видимым слайдам
+	watchSlidesVisibility: true,
+
+	// Зум картинки
+	zoom: {
+		// Макимальное увеличение
+		maxRatio: 5,
+		// Минимальное увеличение
+		minRatio: 1,
+	},
+	// Обновить свайпер
+	// при изменении элементов слайдера
+	observer: true,
+
+	// Обновить свайпер
+	// при изменении родительских
+	// элементов слайдера
+	observeParents: true,
+
+	// Обновить свайпер
+	// при изменении дочерних
+	// элементов слайда
+	observeSlideChildren: true,
+
+});
+
