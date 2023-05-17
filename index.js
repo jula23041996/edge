f4fa84b2aacb8a3a0c0b2269e1e9f6c1d3984c19
jula23041996 <130248094+jula23041996@users.noreply.github.com>
2023-05-17 09:30:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 'use strict'
-// Get the modal
+// CONTACT US
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 if (btn) {
   btn.onclick = function() {
     modal.style.display = "block";
   }
 }
-
-// When the user clicks on <span> (x), close the modal
 if (span) {
   span.onclick = function() {
     modal.style.display = "none";
   }
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (modal && event.target === modal) {
     modal.style.display = "none";
@@ -30,7 +20,7 @@ window.onclick = function(event) {
 }
 
 //SWIPER HOMEPAGE
-// Инициализируем Swiper
+//Swiper1
 let myImageSlider1 = new Swiper('#swiper-container1', {
 	
 	// Стрелки
@@ -39,100 +29,36 @@ let myImageSlider1 = new Swiper('#swiper-container1', {
 		prevEl: '.previous'
 	},
 
-	// Скролл
-
-	// Включение/отключение
-	// перетаскивания на ПК
 	simulateTouch: true,
-	// Чувствительность свайпа
 	touchRatio: 1,
-	// Угол срабатывания свайпа/перетаскивания
 	touchAngle: 45,
-	// Курсор перетаскивания
 	grabCursor: true,
-
-	// Переключение при клике на слайд
 	slideToClickedSlide: false,
-
-	// Навигация по хешу
 	hashNavigation: {
-		// Отслеживать состояние
 		watchState: true,
 	},
-
-	// Управление клавиатурой
 	keyboard: {
-		// Включить\выключить
 		enabled: true,
-		// Включить\выключить
-		// только когда слайдер
-		// в пределах вьюпорта
 		onlyInViewport: true,
-		// Включить\выключить
-		// управление клавишами
-		// pageUp, pageDown
 		pageUpDown: true,
 	},
-
-	// Управление колесом мыши
 	mousewheel: {
-		// Чувствительность колеса мыши
 		sensitivity: 1,
-		// Класс объекта на котором
-		// будет срабатывать прокрутка мышью.
-		//eventsTarget: ".image-slider"
 	},
-
-	// Автовысота
 	autoHeight: false,
-
-	// Количество слайдов для показа
 	slidesPerView: 3,
-	
-	// Отключение функционала
-	// если слайдов меньше чем нужно
 	watchOverflow: true,
-
-	// Отступ между слайдами
 	spaceBetween: 30,
-
-	// Количество пролистываемых слайдов
 	slidesPerGroup: 1,
-
-	// Активный слайд по центру
 	centeredSlides: true,
-
-	// Стартовый слайд.
 	initialSlide: 1,
-
-	// Мультирядность
 	slidesPerColumn: 1,
-
-	// Бесконечный слайдер
 	loop: false,
-
-	// Кол-во дублирующих слайдов
 	loopedSlides: 0,
-
-	// Свободный режим
 	freeMode: true,
-	// Скорость
 	speed: 800,
-
-	// Вертикальный слайдер
 	direction: 'horizontal',
-
-
-	// Эффекты переключения слайдов.
-
-    //Листание
 	effect: 'slide',
-
-	
-	
-	// Брейк поинты (адаптив)
-	// Ширина экрана
-	
 	breakpoints: {
 		320: {
 			slidesPerView: 1,
@@ -147,163 +73,58 @@ let myImageSlider1 = new Swiper('#swiper-container1', {
 			slidesPerView: 3,
 		}
 	},
-	
-	
-	
-/*
-	breakpoints: {
-		'@0.75': {
-			slidesPerView: 1,
-		},
-		'@1.00': {
-			slidesPerView: 1,
-		},
-		'@1.50': {
-			slidesPerView: 3,
-		}
-	},
-*/
-	
-
-	// Отключить предзагрузка картинок
 	preloadImages: false,
-	// Lazy Loading
-	// (подгрузка картинок)
+
 	lazy: {
-		// Подгружать на старте
-		// переключения слайда
 		loadOnTransitionStart: false,
-		// Подгрузить предыдущую
-		// и следующую картинки
 		loadPrevNext: false,
 	},
-	// Слежка за видимыми слайдами
 	watchSlidesProgress: true,
-	// Добавление класса видимым слайдам
 	watchSlidesVisibility: true,
-
-	// Зум картинки
 	zoom: {
-		// Макимальное увеличение
 		maxRatio: 5,
-		// Минимальное увеличение
 		minRatio: 1,
 	},
-
-	// Обновить свайпер
-	// при изменении элементов слайдера
 	observer: true,
-
-	// Обновить свайпер
-	// при изменении родительских
-	// элементов слайдера
 	observeParents: true,
-
-	// Обновить свайпер
-	// при изменении дочерних
-	// элементов слайда
 	observeSlideChildren: true,
 });
-// Инициализируем Swiper
+// Swiper4
 let myImageSlider4 = new Swiper('#swiper-container4', {
-	
-	// Скролл
-
-	// Включение/отключение
-	// перетаскивания на ПК
 	simulateTouch: true,
-	// Чувствительность свайпа
 	touchRatio: 1,
-	// Угол срабатывания свайпа/перетаскивания
 	touchAngle: 45,
-	// Курсор перетаскивания
 	grabCursor: true,
-
-	// Переключение при клике на слайд
 	slideToClickedSlide: false,
-
-	// Навигация по хешу
 	hashNavigation: {
-		// Отслеживать состояние
 		watchState: true,
 	},
-
-	// Управление клавиатурой
 	keyboard: {
-		// Включить\выключить
 		enabled: true,
-		// Включить\выключить
-		// только когда слайдер
-		// в пределах вьюпорта
 		onlyInViewport: true,
-		// Включить\выключить
-		// управление клавишами
-		// pageUp, pageDown
 		pageUpDown: true,
 	},
-
-	// Управление колесом мыши
 	mousewheel: {
-		// Чувствительность колеса мыши
 		sensitivity: 1,
-		// Класс объекта на котором
-		// будет срабатывать прокрутка мышью.
-		//eventsTarget: ".image-slider"
 	},
-
-	// Автовысота
 	autoHeight: false,
-
-	// Количество слайдов для показа
 	slidesPerView: 3,
-	
-	// Отключение функционала
-	// если слайдов меньше чем нужно
 	watchOverflow: true,
-
-	// Отступ между слайдами
 	spaceBetween: 30,
-
-	// Количество пролистываемых слайдов
 	slidesPerGroup: 1,
-
-	// Активный слайд по центру
 	centeredSlides: false,
-
-	// Стартовый слайд.
 	initialSlide: 1,
-
-	// Мультирядность
 	slidesPerColumn: 1,
-
-	// Бесконечный слайдер
 	loop: true,
 	autoplay: {
-		delay: 3000, // Delay between slide transitions (in milliseconds)
-		disableOnInteraction: false, // Autoplay continues even when user interacts with the slider
-	  },
-
-	// Кол-во дублирующих слайдов
+		delay: 3000,
+		disableOnInteraction: false,
+	},
 	loopedSlides: 0,
-
-	// Свободный режим
 	freeMode: true,
-	// Скорость
 	speed: 800,
-
-	// Вертикальный слайдер
 	direction: 'horizontal',
-
-	// Эффекты переключения слайдов.
-
-    //Листание
 	effect: 'slide',
-
-	
-	
-	// Брейк поинты (адаптив)
-	// Ширина экрана
-	
 	breakpoints: {
 		320: {
 			slidesPerView: 1,
@@ -318,61 +139,19 @@ let myImageSlider4 = new Swiper('#swiper-container4', {
 			slidesPerView: 3,
 		}
 	},
-	
-	
-	
-/*
-	breakpoints: {
-		'@0.75': {
-			slidesPerView: 1,
-		},
-		'@1.00': {
-			slidesPerView: 1,
-		},
-		'@1.50': {
-			slidesPerView: 3,
-		}
-	},
-*/
-	
-
-	// Отключить предзагрузка картинок
 	preloadImages: false,
-	// Lazy Loading
-	// (подгрузка картинок)
 	lazy: {
-		// Подгружать на старте
-		// переключения слайда
 		loadOnTransitionStart: false,
-		// Подгрузить предыдущую
-		// и следующую картинки
 		loadPrevNext: false,
 	},
-	// Слежка за видимыми слайдами
 	watchSlidesProgress: true,
-	// Добавление класса видимым слайдам
 	watchSlidesVisibility: true,
-
-	// Зум картинки
 	zoom: {
-		// Макимальное увеличение
 		maxRatio: 5,
-		// Минимальное увеличение
 		minRatio: 1,
 	},
-
-	// Обновить свайпер
-	// при изменении элементов слайдера
 	observer: true,
-
-	// Обновить свайпер
-	// при изменении родительских
-	// элементов слайдера
 	observeParents: true,
-
-	// Обновить свайпер
-	// при изменении дочерних
-	// элементов слайда
 	observeSlideChildren: true,
 });
 
@@ -406,10 +185,8 @@ window.addEventListener("scroll", function() {
 const toggleDivs = document.querySelectorAll('.stage');
 
 function toggleDisplay() {
-  // Get the next sibling element of the clicked div
   const targetDiv = this.firstElementChild;
   const targetDiv2 = this.lastElementChild;
-  // Toggle the display of the target div
   if (targetDiv.style.display === 'none') {
     targetDiv.style.display = 'block';
     targetDiv2.style.display = 'none';
@@ -432,23 +209,17 @@ if (window.matchMedia('(max-width: 1024px)').matches) {
     part.addEventListener('mouseleave', toggleDisplay);
   });
 }
-// Get all the dropdown buttons
 const dropdownButtons2 = document.querySelectorAll('.item__subtitle');
-
-// Loop through the dropdown buttons and add event listeners
 dropdownButtons2.forEach(function(button) {
   button.addEventListener('click', function() {
-    // Get the dropdown content that is next to the button
     const dropdownContent = button.nextElementSibling;
-
-    // Toggle the display of the dropdown content
     dropdownContent.classList.toggle('show');
   });
 });
 
-// Инициализируем Swiper
+// Swiper2
 let myImageSlider2 = new Swiper('#swiper-container2', {
-	// Стрелки
+
 	navigation: {
 		nextEl: '.next2',
 		prevEl: '.previous2'
@@ -472,46 +243,18 @@ let myImageSlider2 = new Swiper('#swiper-container2', {
 	mousewheel: {
 		sensitivity: 1,
 	},
-
-	// Автовысота
 	autoHeight: false,
-
-	// Количество слайдов для показа
 	slidesPerView: 3,
-
-	// Отключение функционала
-	// если слайдов меньше чем нужно
 	watchOverflow: true,
-
-	// Отступ между слайдами
 	spaceBetween: 30,
-
-	// Количество пролистываемых слайдов
 	slidesPerGroup: 1,
-
-	// Активный слайд по центру
 	centeredSlides: true,
-
-	// Стартовый слайд.
 	initialSlide: 1,
-
-	// Мультирядность
 	slidesPerColumn: 1,
-
-	// Бесконечный слайдер
 	loop: false,
-
-	// Кол-во дублирующих слайдов
 	loopedSlides: 0,
-
-	// Свободный режим
 	freeMode: true,
-	
-
-	// Скорость
 	speed: 800,
-
-	// Вертикальный слайдер
 	direction: 'horizontal',
 	effect: 'slide',	
 	breakpoints: {
@@ -522,12 +265,10 @@ let myImageSlider2 = new Swiper('#swiper-container2', {
 			slidesPerView: 1.25,
 		},
 		992: {
-			slidesPerView: 3,
+			slidesPerView: 2.25,
 		},
 	},
 	preloadImages: false,
-	// Lazy Loading
-	// (подгрузка картинок)
 	lazy: {
 		loadOnTransitionStart: false,
 		loadPrevNext: false,
@@ -544,85 +285,44 @@ let myImageSlider2 = new Swiper('#swiper-container2', {
 
 });
 
-// Инициализируем Swiper
 let myImageSlider3 = new Swiper('#swiper-container3', {
-	// Стрелки
 	navigation: {
 		nextEl: '.next3',
 		prevEl: '.previous3'
 	},
-
-	// Включение/отключение
-	// перетаскивания на ПК
 	simulateTouch: true,
-	// Чувствительность свайпа
 	touchRatio: 1,
-	// Угол срабатывания свайпа/перетаскивания
 	touchAngle: 45,
-	// Курсор перетаскивания
 	grabCursor: true,
-
-	// Переключение при клике на слайд
 	slideToClickedSlide: false,
-
-	// Навигация по хешу
 	hashNavigation: {
-		// Отслеживать состояние
 		watchState: true,
 	},
-
-	// Управление клавиатурой
 	keyboard: {
-		// Включить\выключить
 		enabled: true,
 		onlyInViewport: true,
 
 		pageUpDown: true,
 	},
-
-
 	mousewheel: {
 
 		sensitivity: 1,
 
 	},
-
-	// Автовысота
 	autoHeight: false,
-
-	// Количество слайдов для показа
 	slidesPerView: 3,
-
-	// Отключение функционала
-	// если слайдов меньше чем нужно
 	watchOverflow: true,
-
-	// Отступ между слайдами
 	spaceBetween: 30,
-
-	// Количество пролистываемых слайдов
 	slidesPerGroup: 1,
-
-	// Активный слайд по центру
 	centeredSlides: true,
-
-	// Стартовый слайд.
 	initialSlide: 1,
-
-	// Мультирядность
 	slidesPerColumn: 1,
-
-	// Бесконечный слайдер
 	loop: false,
-
-	// Кол-во дублирующих слайдов
 	loopedSlides: 0,
-
 	freeMode: true,
-	
-
-
-	speed: 800,	
+	speed: 800,
+	direction: 'horizontal',
+	effect: 'slide',	
 	direction: 'horizontal',
 	effect: 'slide',	
 	breakpoints: {
@@ -633,37 +333,22 @@ let myImageSlider3 = new Swiper('#swiper-container3', {
 			slidesPerView: 1.25,
 		},
 		992: {
-			slidesPerView: 3,
+			slidesPerView: 2.25,
 		},
 	},
-
 	preloadImages: false,
-
 	lazy: {
-
 		loadOnTransitionStart: false,
-
 		loadPrevNext: false,
 	},
-
 	watchSlidesProgress: true,
-
 	watchSlidesVisibility: true,
-
-
 	zoom: {
-
 		maxRatio: 5,
-
 		minRatio: 1,
 	},
-
 	observer: true,
-
-
 	observeParents: true,
-
-
 	observeSlideChildren: true,
 
 });
